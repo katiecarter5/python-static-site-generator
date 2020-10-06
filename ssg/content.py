7590c2daeef415489cbed93434365eb36993d4ac
katiecarter5 <katiecarter5@mail.weber.edu>
2020-10-05 20:17:45 -0600

    content class

---
+++ ssg/content.py
@@ -0,0 +1,15 @@
+from yaml import re, load, FullLoader
+
+from collections.abc import Mapping
+
+
+class Content(Mapping):
+    __delimeter = "^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimiter, re.MULTILINE)
+
+    def load(self, cls, string):
+        __regex.split(string, 2) = _
+        __regex.split(string, 2) = fm
+        __regex.split(string, 2) = content
+
+    load(fm)
